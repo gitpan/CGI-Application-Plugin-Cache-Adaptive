e0c11bfa7927116e1f2795388c6a83fb2a55ce8d
Toru Yamaguchi <trymch+public@gmail.com>
2007-05-18 11:40:15 +0000

    Import of ZIGOROU/CGI-Application-Plugin-Cache-Adaptive-0.02 from CPAN.
    gitpan-cpan-distribution: CGI-Application-Plugin-Cache-Adaptive
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ZIGOROU/CGI-Application-Plugin-Cache-Adaptive-0.02.tar.gz
    gitpan-cpan-author:       ZIGOROU
    gitpan-cpan-maturity:     released

--- lib/CGI/Application/Plugin/Cache/Adaptive.pm
+++ lib/CGI/Application/Plugin/Cache/Adaptive.pm
@@ -18,11 +18,11 @@ CGI::Application::Plugin::Cache::Adaptive - Provide cacheable to method using at
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
